402a7c7612ec314c0fdc6be9758b40ee171c8f02
takecchi <takecchi.kobayashi@gmail.com>
2024-06-19 14:52:49 +0900

    fix for lint

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { SharedModule } from "./shared/shared.module";
+import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [
@@ -10,7 +10,7 @@ import { SharedModule } from "./shared/shared.module";
       isGlobal: true,
       envFilePath: ['.env.production', '.env.develop', '.env.local', '.env'],
     }),
-    SharedModule
+    SharedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
